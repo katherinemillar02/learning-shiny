61ce1e72e2dfb8fde7298024eb954eee80f638d1
Katie Millar <ajh20fhu@uea.ac.uk>
2023-06-16 15:12:35 +0100

    writing code for chapter 5

---
+++ C5.R
@@ -0,0 +1,20 @@
+# shiny workflow 
+
+# loading the packages in 
+library(shiny)
+
+# the ui 
+ui <- fluidPage(
+  
+)
+
+# loading the server 
+server <- function(input, output, session) {
+  
+}
+
+# loading the app
+shinyApp(ui, server)
+
+# this is how one can start the basis of a shiny app 
+
